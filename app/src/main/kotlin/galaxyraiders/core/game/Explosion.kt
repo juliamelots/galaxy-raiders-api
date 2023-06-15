9e2a5e89d7b196d905bb2683144430340a021233
Julia Melo T. dos Santos <juliamelots@usp.br>
2023-06-14 21:03:09 -0300

    Make Explosion be visible in web
    Signed-off-by: Julia Melo T. dos Santos <juliamelots@usp.br>

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -3,12 +3,12 @@ package galaxyraiders.core.game
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
-const val EXPLOSION_LIFE_TIME: Int = 30
+const val EXPLOSION_LIFE_TIME: Int = 15
 
 class Explosion(
-  initialPosition: Point2D,
+  initialPosition: Point2D
 ) :
-  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0, 0.0), 0.0, 0.0) {
+  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0, 0.0), 2.0, 2.0) {
   var isTriggered: Boolean = false
 
   var timer: Int = EXPLOSION_LIFE_TIME
